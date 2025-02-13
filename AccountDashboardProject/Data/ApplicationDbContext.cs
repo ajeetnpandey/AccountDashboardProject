8b6c425454c509d8c9872e0b093e938bc3b0318c
Ajeet Pandey <phibonacci_ajeetpandey@outlook.com>
2025-02-13 10:52:36 +0530

    Added identityservice

---
+++ AccountDashboardProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AccountDashboardProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountDashboardProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
